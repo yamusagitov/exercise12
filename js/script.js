c7840f82747179bc0b27e4d91730aef9b2224001
IURII <yamusagitov@gmail.com>
2019-07-28 17:42:06 +0500

    exercise11

--- js/script.js
+++ js/script.js
@@ -81,7 +81,43 @@ window.addEventListener('DOMContentLoaded', function () {
             }
         }
     }
-
     setClock('timer', deadline);
+
+
+    // Модальное окно ("Узнать больше")
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    more.addEventListener('click', function () {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    })
+
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    })
+
+    // Модальное окно ("Узнать подробнее")
+
+    let learnMore = document.querySelectorAll('.description-btn');
+    
+    for (let i = 0; i < learnMore.length; i++) {
+        learnMore[i].addEventListener('click', function () {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        })
+
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    })
+}
 
 });
